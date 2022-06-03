773e57a8b7ec2bca4e234dd554adaa396a4720a8
Kayden Lee <kayden@programmer.net>
2022-06-03 19:41:03 +0800

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -20,7 +20,7 @@ const roomId = getRoomId();
 const peerInfo = getPeerInfo();
 const peerLoockupUrl = 'https://extreme-ip-lookup.com/json/?key=demo2'; // get your API Key at https://extreme-ip-lookup.com
 const avatarApiUrl = 'https://eu.ui-avatars.com/api';
-const surveyURL = 'https://www.questionpro.com/t/AUs7VZq00L';
+const surveyURL = 'https://forms.gle/Ca4wkZQmtibyVt1D9';
 const welcomeImg = '../images/image-placeholder.png';
 const shareUrlImg = '../images/image-placeholder.png';
 const leaveRoomImg = '../images/leave-room.png';
